f3cdd985794d3b9efc1f8734f3d74f3d100c46c9
jeffclaybrook <48254646+jeffclaybrook@users.noreply.github.com>
2022-04-12 18:48:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,184 +1,185 @@
 let songs = [
    {
-      name: 'Sherane a.k.a Master Splinters Daughter',
-      artist: 'Kendrick Lamar',
-      duration: '04:32',
-      file: 'songs/song-1.mp3',
-      cover: 'images/image-1.webp'
+      name: "Sherane a.k.a Master Splinter's Daughter",
+      artist: "Kendrick Lamar",
+      duration: "04:32",
+      file: "songs/song-1.mp3",
+      cover: "images/image-1.webp"
    },
    {
-      name: 'Bitch, Dont Kill My Vibe',
-      artist: 'Kendrick Lamar',
-      duration: '05:10',
-      file: 'songs/song-2.mp3',
-      cover: 'images/image-2.webp'
+      name: "Bitch, Don't Kill My Vibe",
+      artist: "Kendrick Lamar",
+      duration: "05:10",
+      file: "songs/song-2.mp3",
+      cover: "images/image-2.webp"
    },
    {
-      name: 'Backseat Freestyle',
-      artist: 'Kendrick Lamar',
-      duration: '03:32',
-      file: 'songs/song-3.mp3',
-      cover: 'images/image-3.webp'
+      name: "Backseat Freestyle",
+      artist: "Kendrick Lamar",
+      duration: "03:32",
+      file: "songs/song-3.mp3",
+      cover: "images/image-3.webp"
    },
    {
-      name: 'The Art of Peer Pressure',
-      artist: 'Kendrick Lamar',
-      duration: '05:24',
-      file: 'songs/song-4.mp3',
-      cover: 'images/image-4.webp'
+      name: "The Art of Peer Pressure",
+      artist: "Kendrick Lamar",
+      duration: "05:24",
+      file: "songs/song-4.mp3",
+      cover: "images/image-4.webp"
    },
    {
-      name: 'Money Trees (feat. Jay Rock)',
-      artist: 'Kendrick Lamar',
-      duration: '06:26',
-      file: 'songs/song-5.mp3',
-      cover: 'images/image-5.webp'
+      name: "Money Trees",
+      artist: "Kendrick Lamar feat. Jay Rock",
+      duration: "06:26",
+      file: "songs/song-5.mp3",
+      cover: "images/image-5.webp"
    },
    {
-      name: 'Poetic Justice (feat. Drake)',
-      artist: 'Kendrick Lamar',
-      duration: '05:00',
-      file: 'songs/song-6.mp3',
-      cover: 'images/image-6.webp'
+      name: "Poetic Justice",
+      artist: "Kendrick Lamar feat. Drake",
+      duration: "05:00",
+      file: "songs/song-6.mp3",
+      cover: "images/image-6.webp"
    },
    {
-      name: 'Good Kid',
-      artist: 'Kendrick Lamar',
-      duration: '03:34',
-      file: 'songs/song-7.mp3',
-      cover: 'images/image-7.webp'
+      name: "Good Kid",
+      artist: "Kendrick Lamar",
+      duration: "03:34",
+      file: "songs/song-7.mp3",
+      cover: "images/image-7.webp"
    },
    {
-      name: 'm.A.A.d city (feat. MC Eiht)',
-      artist: 'Kendrick Lamar',
-      duration: '05:50',
-      file: 'songs/song-8.mp3',
-      cover: 'images/image-8.webp'
+      name: "m.A.A.d city",
+      artist: "Kendrick Lamar feat. MC Eiht",
+      duration: "05:50",
+      file: "songs/song-8.mp3",
+      cover: "images/image-8.webp"
    },
    {
-      name: 'Swimming Pools (Drank) [Extended Version]',
-      artist: 'Kendrick Lamar',
-      duration: '05:13',
-      file: 'songs/song-9.mp3',
-      cover: 'images/image-9.webp'
+      name: "Swimming Pools (Drank)",
+      artist: "Kendrick Lamar",
+      duration: "05:13",
+      file: "songs/song-9.mp3",
+      cover: "images/image-9.webp"
    },
    {
-      name: 'Sing About Me, Im Dying of Thirst',
-      artist: 'Kendrick Lamar',
-      duration: '12:03',
-      file: 'songs/song-10.mp3',
-      cover: 'images/image-10.webp'
+      name: "Sing About Me, I'm Dying of Thirst",
+      artist: "Kendrick Lamar",
+      duration: "12:03",
+      file: "songs/song-10.mp3",
+      cover: "images/image-10.webp"
    },
    {
-      name: 'Real (feat. Anna Wise)',
-      duration: '07:23',
-      artist: 'Kendrick Lamar',
-      file: 'songs/song-11.mp3',
-      cover: 'images/image-11.webp'
+      name: "Real",
+      artist: "Kendrick Lamar feat. Anna Wise",
+      duration: "07:23",
+      file: "songs/song-11.mp3",
+      cover: "images/image-11.webp"
    },
    {
-      name: 'Compton (feat. Dr. Dre)',
-      artist: 'Kendrick Lamar',
-      duration: '04:08',
-      file: 'songs/song-12.mp3',
-      cover: 'images/image-12.webp'
+      name: "Compton",
+      artist: "Kendrick Lamar feat. Dr. Dre",
+      duration: "04:08",
+      file: "songs/song-12.mp3",
+      cover: "images/image-12.webp"
    },
    {
-      name: 'All We Got (feat. Kanye West & Chicago Childrens Choir)',
-      artist: 'Chance the Rapper',
-      duration: '03:23',
-      file: 'songs/song-13.mp3',
-      cover: 'images/image-13.webp'
+      name: "All We Got",
+      artist: "Chance the Rapper feat. Kanye West & Chicago Childrens Choir",
+      duration: "03:23",
+      file: "songs/song-13.mp3",
+      cover: "images/image-13.webp"
    },
    {
-      name: 'No Problem (feat. Lil Wayne & 2 Chainz)',
-      artist: 'Chance the Rapper',
-      duration: '05:04',
-      file: 'songs/song-14.mp3',
-      cover: 'images/image-14.webp'
+      name: "No Problem",
+      artist: "Chance the Rapper feat. Lil Wayne & 2 Chainz",
+      duration: "05:04",
+      file: "songs/song-14.mp3",
+      cover: "images/image-14.webp"
    },
    {
-      name: 'Summer Friends (feat. Jeremih & Francis & The Lights)',
-      artist: 'Chance the Rapper',
-      duration: '04:50',
-      file: 'songs/song-15.mp3',
-      cover: 'images/image-15.webp'
+      name: "Summer Friends",
+      artist: "Chance the Rapper feat. Jeremih & Francis & The Lights",
+      duration: "04:50",
+      file: "songs/song-15.mp3",
+      cover: "images/image-15.webp"
    },
    {
-      name: 'D.R.A.M. Sings Special',
-      artist: 'Chance the Rapper',
-      duration: '01:41',
-      file: 'songs/song-16.mp3',
-      cover: 'images/image-16.webp'
+      name: "D.R.A.M. Sings Special",
+      artist: "Chance the Rapper",
+      duration: "01:41",
+      file: "songs/song-16.mp3",
+      cover: "images/image-16.webp"
    },
    {
-      name: 'Blessings (feat. Jamila Woods)',
-      artist: 'Chance the Rapper',
-      duration: '03:42',
-      file: 'songs/song-17.mp3',
-      cover: 'images/image-17.webp'
+      name: "Blessings",
+      artist: "Chance the Rapper feat. Jamila Woods",
+      duration: "03:42",
+      file: "songs/song-17.mp3",
+      cover: "images/image-17.webp"
    },
    {
-      name: 'Same Drugs',
-      artist: 'Chance the Rapper',
-      duration: '04:17',
-      file: 'songs/song-18.mp3',
-      cover: 'images/image-18.webp'
+      name: "Same Drugs",
+      artist: "Chance the Rapper",
+      duration: "04:17",
+      file: "songs/song-18.mp3",
+      cover: "images/image-18.webp"
    },
    {
-      name: 'Mixtape (feat. Young Thug & Lil Yachty)',
-      artist: 'Chance the Rapper',
-      duration: '04:52',
-      file: 'songs/song-19.mp3',
-      cover: 'images/image-19.webp'
+      name: "Mixtape",
+      artist: "Chance the Rapper feat. Young Thug & Lil Yachty",
+      duration: "04:52",
+      file: "songs/song-19.mp3",
+      cover: "images/image-19.webp"
    },
    {
-      name: 'Angels (feat. Saba)',
-      artist: 'Chance the Rapper',
-      duration: '03:26',
-      file: 'songs/song-20.mp3',
-      cover: 'images/image-20.webp'
+
+      name: "Angels",
+      artist: "Chance the Rapper feat. Saba",
+      duration: "03:26",
+      file: "songs/song-20.mp3",
+      cover: "images/image-20.webp"
    },
    {
-      name: 'Juke Jam (feat. Justin Bieber & Towkio)',
-      artist: 'Chance the Rapper',
-      duration: '03:39',
-      file: 'songs/song-21.mp3',
-      cover: 'images/image-21.webp'
+      name: "Juke Jam",
+      artist: "Chance the Rapper",
+      duration: "03:39",
+      file: "songs/song-21.mp3",
+      cover: "images/image-21.webp"
    },
    {
-      name: 'All Night (feat. Knox Fortune)',
-      artist: 'Chance the Rapper',
-      duration: '02:21',
-      file: 'songs/song-22.mp3',
-      cover: 'images/image-22.webp'
+      name: "All Night",
+      artist: "Chance the Rapper feat. Justin Bieber & Towkio",
+      duration: "02:21",
+      file: "songs/song-22.mp3",
+      cover: "images/image-22.webp"
    },
    {
-      name: 'How Great (feat. Jay Electronica & My cousin Nicole)',
-      artist: 'Chance the Rapper',
-      duration: '05:37',
-      file: 'songs/song-23.mp3',
-      cover: 'images/image-23.webp'
+      name: "How Great",
+      artist: "Chance the Rapper feat. Jay Electronica & My cousin Nicole",
+      duration: "05:37",
+      file: "songs/song-23.mp3",
+      cover: "images/image-23.webp"
    },
    {
-      name: 'Smoke Break (feat. Future)',
-      artist: 'Chance the Rapper',
-      duration: '03:46',
-      file: 'songs/song-24.mp3',
-      cover: 'images/image-24.webp'
+      name: "Smoke Break",
+      artist: "Chance the Rapper feat. Future",
+      duration: "03:46",
+      file: "songs/song-24.mp3",
+      cover: "images/image-24.webp"
    },
    {
-      name: 'Finish Line / Drown (feat. T-Pain, Kirk Franklin, Eryn Allen Kane & Noname)',
-      artist: 'Chance the Rapper',
-      duration: '06:47',
-      file: 'songs/song-25.mp3',
-      cover: 'images/image-25.webp'
+      name: "Finish Line / Drown",
+      artist: "Chance the Rapper feat. T-Pain, Kirk Franklin, Eryn Allen Kane & Noname",
+      duration: "06:47",
+      file: "songs/song-25.mp3",
+      cover: "images/image-25.webp"
    },
    {
-      name: 'Blessings (feat. Ty Dolla $ign, Raury, BJ The Chicago Kid & Anderson .Paak)',
-      artist: 'Chance the Rapper',
-      duration: '03:50',
-      file: 'songs/song-26.mp3',
-      cover: 'images/image-26.webp'
+      name: "Blessings",
+      artist: "Chance the Rapper feat. Ty Dolla $ign, Raury, BJ The Chicago Kid & Anderson .Paak",
+      duration: "03:50",
+      file: "songs/song-26.mp3",
+      cover: "images/image-26.webp"
    }
 ]
